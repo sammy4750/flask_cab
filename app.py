a5d74009acf184fc2d05501f0ffd82c3a53859ad
sammy4750 <saumyashah4750@gmail.com>
2022-01-03 19:57:48 -0800

    Theme integration by sammy

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/booking')
+def booking():
+    return render_template("booking.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/services')
+def services():
+    return render_template("services.html")
