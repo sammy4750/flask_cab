3eb74cb69b987776acea5140dd7e27e74183f535
sammy4750 <saumyashah4750@gmail.com>
2022-01-07 19:11:14 -0800

    Login almost done

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 from flask import Flask, render_template, request, redirect, flash
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-# from flask_login import LoginManager
-# from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
-# login_manager = LoginManager()
+#flask_login Stuff:
 
-class users(db.Model):
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+
+    return users.query.get(int(user_id))
+
+#  Database Models
+
+class users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(50), nullable=False)
     lname = db.Column(db.String(50), nullable=False)
@@ -28,9 +42,15 @@ class users(db.Model):
 
 @app.route('/')
 def index():
+    if request.cookies:
+        c = request.cookies
+        print(c)
+    else:
+        print("Bye Bye")
     return render_template("index.html")
 
 @app.route('/booking')
+@login_required
 def booking():
     return render_template("booking.html")
 
@@ -42,25 +62,7 @@ def contact():
 def about():
     return render_template("about.html")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-
-        data = users.query.filter_by(email=email).first()
-
-        # if data is not None:              #code by sammy for login
-        #     user_pass = data[5]
-        #     if password == user_pass:
-        #         return redirect("/")   
-
-        if not data or not check_password_hash(data.password, password):
-            flash('Please check your login credentials')
-            return redirect('/login')
-        return redirect("/")
-
-    return render_template("login.html")
+# Register, Login and Logout
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -84,6 +86,30 @@ def register():
         return redirect("/login")
 
     return render_template("register.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        data = users.query.filter_by(email=email).first()
+
+        if not data or not check_password_hash(data.password, password):
+            flash('Please check your login credentials')
+            return redirect('/login')
+        login_user(data)
+        return render_template("index.html", data=data)
+
+    return render_template("login.html")
+
+@app.route('/logout',methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+# Run Code
 
 if __name__=="__main__":
     app.run(debug=True)
